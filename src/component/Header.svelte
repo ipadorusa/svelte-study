8b3eaf6994a227772278fbd4ca5b3c5229039ba2
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2019-12-06 23:38:05 +0900

    몰해볼까

---
+++ src/component/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  const menuList = [
+    {menu:'Home', url:''},
+    {menu:'Profile', url:''},
+    {menu:'Article', url:''},
+  ]
+</script>
+<header id="header">
+  <ul>
+  {#each menuList as header}
+    <li>{header.menu}</li>
+  {/each}
+  </ul>
+</header>
